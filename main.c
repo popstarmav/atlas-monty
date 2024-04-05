e41791cbafb2e6ad28dd3e020d4c1e752a8c2e8c
root <root@MrSlim3.>
2024-04-05 17:25:12 -0500

    monty

--- main.c
+++ main.c
@@ -3,51 +3,66 @@
 #include <string.h>
 #include "monty.h"
 
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        fprintf(stderr, "USAGE: monty file\n");
-        exit(EXIT_FAILURE);
-    }
+int main(int argc, char *argv[])
+{
+	FILE *file;
+	char line[100];
+	char *opcode = strtok(line, " \n");
+	char *arg = strtok(NULL, " \n");
+	char *endptr;
+	long int value = strtol(arg, &endptr, 10);
+	unsigned int line_number = 0;
+	stack_t **stack = NULL;
 
-    FILE *file = fopen(argv[1], "r");
-    if (file == NULL) {
-        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-        exit(EXIT_FAILURE);
-    }
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
 
-    char line[100];
-    int line_number = 0;
-    while (fgets(line, sizeof(line), file)) {
-        line_number++;
+	file = fopen(argv[1], "r");
+	if (file == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
 
-        char *opcode = strtok(line, " \n");
-        if (opcode == NULL || strncmp(opcode, "#", 1) == 0) {
-            continue;
-        }
+	while (fgets(line, sizeof(;ine), file))
+	{
+		line_number++;
 
-        if (strcmp(opcode, "push") == 0) {
-            char *arg = strtok(NULL, " \n");
-            if (arg == NULL) {
-                fprintf(stderr, "L%d: usage: push integer\n", line_number);
-                fclose(file);
-                exit(EXIT_FAILURE);
-            }
+		if (opcode == NULL || strncmp(opcode, "#", 1) 1== 0)
+			continue;
 
-            char *endptr;
-            long int value = strtol(arg, &endptr, 10);
-            if (*endptr != '\0') {
-                fprintf(stderr, "L%d: usage: push integer\n", line_number);
-                fclose(file);
-                exit(EXIT_FAILURE);
-            }
-            push((int)value);
-        } else if (strcmp(opcode, "pall") == 0) {
-            pall();
-        } else {
-            fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
-        }
-    }
+		if (strcmp(opcode, "push") == 0)
+		{
+			if (arg == NULL)
+			{
+				frprintf(stderr, "L%d: usage: push integer\n", line_number);
+				fclose(file);
+				exit(EXIT_FAILURE);
+			}
 
-    fclose(file);
-    return 0;
+			if (*endptr != '\0)
+			{
+				fprintf(stderr, "L%d: usage: push interger\n", line_number);
+				fclose(file);
+				exit(EXIT_FAILURE);
+			}
+			push(stack, (int)value, line_number);
+
+		}
+		else if (strcmp(opcode, "pall") == 0)
+		{
+			pall(stack, line_number);
+		}
+		else
+		{
+			fprintf(stderr, "L%d: unknown instruction %\n", line_number, opcide);
+		}
+	}
+
+	flcose(file);
+	return(0);
 }
+
