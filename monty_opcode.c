2ce19964f7468c621c7b88e3597a50fe5fefd22f
root <root@MrSlim3.>
2024-04-08 21:46:51 -0500

    monty

--- monty_opcode.c
+++ monty_opcode.c
@@ -1,13 +1,13 @@
 #include "monty.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 #include <string.h>
 
 void process_opcode(stack_t **stack, char *opcode, char *arg, unsigned int line_number) {
     if (strcmp(opcode, "push") == 0) {
-        /* Push operation */
-        if (!arg) {
-            fprintf(stderr, "Error: L%u: usage: push integer\n", line_number);
+        if (!arg || !isdigit(*arg)) {
+            fprintf(stderr, "L%u: usage: push integer\n", line_number);
             exit(EXIT_FAILURE);
         }
         int value = atoi(arg);
@@ -19,25 +19,14 @@ void process_opcode(stack_t **stack, char *opcode, char *arg, unsigned int line_
         new_node->n = value;
         new_node->next = *stack;
         *stack = new_node;
-    } else if (strcmp(opcode, "pop") == 0) {
-        /* Pop operation */
-        if (*stack == NULL) {
-            fprintf(stderr, "Error: L%u: can't pop an empty stack\n", line_number);
-            exit(EXIT_FAILURE);
-        }
-        stack_t *temp = *stack;
-        *stack = (*stack)->next;
-        free(temp);
     } else if (strcmp(opcode, "pall") == 0) {
-        /* Print all elements of the stack */
         stack_t *current = *stack;
         while (current != NULL) {
             printf("%d\n", current->n);
             current = current->next;
         }
     } else {
-        fprintf(stderr, "Error: L%u: unknown instruction %s\n", line_number, opcode);
+        fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
         exit(EXIT_FAILURE);
     }
 }
-
